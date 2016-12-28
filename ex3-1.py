5c0bfd73557bf108e8bf288d2f4a4e365f04f651
lborodynamo <ronnie.tian@a-carbon.com>
2016-12-28 21:32:32 +0800

    Add files via upload

---
+++ ex3-1.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+print 2-1/4
+print 2-3/4
+print 2-5/4
+print 2-5.0/4
+print 2-2.0/4
+print 2-2/4.0
+print 4%2
+print 4.0%2
+print "Hey %s there." % "you"
---
+++ ex5-4.py
@@ -0,0 +1,6 @@
+height_cm = 170
+weight_kg = 65
+height_in = height_cm * 0.394
+weight_pd = weight_kg * 2.205
+print "I'm %d inches tall." % height_in
+print "I'm %d pounds heavy." % weight_pd
